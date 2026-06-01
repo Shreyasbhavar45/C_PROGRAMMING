5d24be9c6e9af2558b5cc6d4bc3c7f5b62e97229
Shreyas Rajaram Bhavar <shreyasbhavar0@gmail.com>
2026-06-01 15:48:05 +0530

    changes

---
+++ chapter1/01_variable.c
@@ -0,0 +1,12 @@
+// print the integerr variable //
+#include <stdio.h>
+
+int main(){
+    
+    int a;//variable declaration
+    a = 6;//variable initialization
+
+    printf("%d",a);
+
+    return 0;
+}
