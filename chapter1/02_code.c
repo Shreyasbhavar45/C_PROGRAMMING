59c06eed072a88a62b0a47b004f22c217bdcffdb
Shreyas Rajaram Bhavar <shreyasbhavar0@gmail.com>
2026-06-06 23:18:46 +0530

    new code

---
+++ chapter1/02_code.c
@@ -0,0 +1,13 @@
+//float//
+#include <stdio.h>
+int main()
+{
+    // float is use to store the data type float//
+  float a = 5 ;
+  float b = 6 ;
+
+  // "%f" is the char that we use to float
+  printf("%f\n",a+b);
+  
+  return 0;
+}
---
+++ chapter1/03_code.c
@@ -0,0 +1,16 @@
+//Arithemetic Operator//
+#include <stdio.h>
+int main () 
+{
+    int a = 6;
+    int b = 3;
+
+    printf("addition of two number:- %d\n",a+b);    
+    printf("subtraction of two number:- %d\n",a-b);    
+    printf("Multiplication of two number:- %d\n",a*b);    
+    printf("Division of two number:- %d\n",a/b);    
+        
+    
+    return 0 ;
+}
+
